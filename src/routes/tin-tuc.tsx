67246a45aeb652d14ed9853be7b2122d1e5efd14
tran.thien <tran.thien1830@.com>
2026-07-24 18:59:42 +0700

    Permanently delete ve-chung-toi and tin-tuc routes

--- src/routes/tin-tuc.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/tin-tuc")({
-  beforeLoad: () => {
-    throw redirect({ to: "/", replace: true });
-  },
-});
